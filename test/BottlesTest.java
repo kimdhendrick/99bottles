87ec360eabc108a776f4ae048f734e2c26de7741
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-23 13:27:07 -0700

    Listing 2.3: Verse 3 Test

--- test/BottlesTest.java
+++ test/BottlesTest.java
@@ -12,4 +12,14 @@ public class BottlesTest {
 
         assertEquals(expected, new Bottles().verse(99));
     }
+
+    @Test
+    public void testAnotherVerse() {
+        String expected = "3 bottles of beer on the wall, " +
+                "3 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "2 bottles of beer on the wall.\n";
+
+        assertEquals(expected, new Bottles().verse(3));
+    }
 }
