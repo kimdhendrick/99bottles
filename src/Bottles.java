6e0ac69dc42457853ea178a8994b831e9adf3fa4
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-23 14:02:17 -0700

    Listing 2.15: Verses 99 98 Literal

--- src/Bottles.java
+++ src/Bottles.java
@@ -1,4 +1,16 @@
 class Bottles {
+    public String verses(int number1, int number2) {
+        return "99 bottles of beer on the wall, " +
+                "99 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "98 bottles of beer on the wall.\n" +
+                "\n" +
+                "98 bottles of beer on the wall, " +
+                "98 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "97 bottles of beer on the wall.\n";
+    }
+
     public String verse(int number) {
         switch (number) {
             case 0: {
