fa9b68955c25762ec33ee5998b0e431ae89bd913
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-23 14:07:47 -0700

    Listing 2.18: Verses 2, 1, 0 Test

--- test/BottlesTest.java
+++ test/BottlesTest.java
@@ -67,4 +67,24 @@ public class BottlesTest {
 
         assertEquals(expected, new Bottles().verses(99, 98));
     }
+
+    @Test
+    public void testAFewVerses() {
+        String expected = "2 bottles of beer on the wall, " +
+                "2 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "1 bottle of beer on the wall.\n" +
+                "\n" +
+                "1 bottle of beer on the wall, " +
+                "1 bottle of beer.\n" +
+                "Take it down and pass it around, " +
+                "no more bottles of beer on the wall.\n" +
+                "\n" +
+                "No more bottles of beer on the wall, " +
+                "no more bottles of beer.\n" +
+                "Go to the store and buy some more, " +
+                "99 bottles of beer on the wall.\n";
+
+        assertEquals(expected, new Bottles().verses(2, 0));
+    }
 }
