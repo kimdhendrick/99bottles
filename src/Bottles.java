785a5de5cdec953048aaa315a49323143ef88130
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-23 13:07:53 -0700

    Listing 2.2: Verse 99 Code

--- src/Bottles.java
+++ src/Bottles.java
@@ -1,5 +1,8 @@
 class Bottles {
     public String verse(int number) {
-        return null;
+        return "99 bottles of beer on the wall, " +
+                "99 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "98 bottles of beer on the wall.\n";
     }
 }
