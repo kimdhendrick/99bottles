3ca7e43d8e6c8dcbfa753713d25d0e3ccda7bfdf
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-23 13:47:58 -0700

    Listing 2.14: Verses 99 98 Test

--- test/BottlesTest.java
+++ test/BottlesTest.java
@@ -52,4 +52,19 @@ public class BottlesTest {
 
         assertEquals(expected, new Bottles().verse(0));
     }
+
+    @Test
+    public void testACoupleVerses() {
+        String expected = "99 bottles of beer on the wall, " +
+                "99 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "98 bottles of beer on the wall.\n" +
+                "\n" +
+                "98 bottles of beer on the wall, " +
+                "98 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "97 bottles of beer on the wall.\n";
+
+        assertEquals(expected, new Bottles().verses(99, 98));
+    }
 }
