2a2773df4d4f9cb783aef02fb8afe3606b84b39b
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-23 13:28:30 -0700

    Listing 2.4: Conditional

--- src/Bottles.java
+++ src/Bottles.java
@@ -1,8 +1,15 @@
 class Bottles {
     public String verse(int number) {
-        return "99 bottles of beer on the wall, " +
-                "99 bottles of beer.\n" +
-                "Take one down and pass it around, " +
-                "98 bottles of beer on the wall.\n";
+        if (number == 99) {
+            return "99 bottles of beer on the wall, " +
+                    "99 bottles of beer.\n" +
+                    "Take one down and pass it around, " +
+                    "98 bottles of beer on the wall.\n";
+        } else {
+            return "3 bottles of beer on the wall, " +
+                    "3 bottles of beer.\n" +
+                    "Take one down and pass it around, " +
+                    "2 bottles of beer on the wall.\n";
+        }
     }
 }
