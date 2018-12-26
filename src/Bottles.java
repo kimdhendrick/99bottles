2a98b9c3493a81b8d5a3d15130fea1eb594cc5ee
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-25 21:12:30 -0700

    Listing 3.12b: Sparse Container Used in Else Branch

--- src/Bottles.java
+++ src/Bottles.java
@@ -37,12 +37,12 @@ class Bottles {
                 return number + " bottles of beer on the wall, " +
                         number + " bottles of beer.\n" +
                         "Take one down and pass it around, " +
-                        (number - 1) + " " + container() + " of beer on the wall.\n";
+                        (number - 1) + " " + container(number - 1) + " of beer on the wall.\n";
             }
         }
     }
 
-    private String container(int number) {
+    private String container() {
         return "bottles";
     }
 }
