8df1e6e4a97c42a36fbf41ea7e9f416fd8e4bdbb
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-29 15:26:56 -0700

    Listing 4.49: Consistent Abstractions

--- src/Bottles.java
+++ src/Bottles.java
@@ -47,13 +47,10 @@ class Bottles {
     }
 
     private String quantity(int number) {
-        switch (number) {
-            case -1:
-                return "99";
-            case 0:
-                return "no more";
-            default:
-                return String.valueOf(number);
+        if (number == 0) {
+            return "no more";
+        } else {
+            return String.valueOf(number);
         }
     }
 
