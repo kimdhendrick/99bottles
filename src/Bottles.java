4c7fd1a17b3f4a5d993030e59da8717e55f6f451
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-29 15:29:46 -0700

    Listing 4.53: Case 0 Sends Container

--- src/Bottles.java
+++ src/Bottles.java
@@ -19,7 +19,7 @@ class Bottles {
                 return quantity(number).substring(0, 1).toUpperCase() + quantity(number).substring(1) + " " + container(number) + " of beer on the wall, " +
                         quantity(number) + " " + container(number) + " of beer.\n" +
                         action(number) + ", " +
-                        quantity(99) + " bottles of beer on the wall.\n";
+                        quantity(99) + " " + container(number - 1) + " of beer on the wall.\n";
             }
             default: {
                 return quantity(number).substring(0, 1).toUpperCase() + quantity(number).substring(1) + " " + container(number) + " of beer on the wall, " +
