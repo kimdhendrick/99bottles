5221a84faa0ba2604ebac11ff286089bed2b4622
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-29 15:21:13 -0700

    Listing 4.42: 2nd Phrases Send Quantity and Container

--- src/Bottles.java
+++ src/Bottles.java
@@ -17,13 +17,13 @@ class Bottles {
         switch (number) {
             case 0: {
                 return quantity(number).substring(0, 1).toUpperCase() + quantity(number).substring(1) + " " + container(number) + " of beer on the wall, " +
-                        "no more bottles of beer.\n" +
+                        quantity(number) + " " + container(number) + " of beer.\n" +
                         "Go to the store and buy some more, " +
                         "99 bottles of beer on the wall.\n";
             }
             default: {
                 return quantity(number).substring(0, 1).toUpperCase() + quantity(number).substring(1) + " " + container(number) + " of beer on the wall, " +
-                        number + " " + container(number) + " of beer.\n" +
+                        quantity(number) + " " + container(number) + " of beer.\n" +
                         "Take " + pronoun(number) + " down and pass it around, " +
                         quantity(number - 1) + " " + container(number - 1) + " of beer on the wall.\n";
             }
