3245a8e3bc843c06f0df2b9b4c56e0eacc09070b
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-30 11:19:09 -0700

    Listing 5.2: Various Conditional Forms

--- src/Bottles.java
+++ src/Bottles.java
@@ -20,6 +20,7 @@ class Bottles {
                 quantity(successor(number)) + " " + container(successor(number)) + " of beer on the wall.\n";
     }
 
+    // verbose conditional
     private String container(int number) {
         if (number == 1) {
             return "bottle";
@@ -28,20 +29,18 @@ class Bottles {
         }
     }
 
+    // ternary expression
     private String pronoun(int number) {
-        if (number == 1) {
-            return "it";
-        } else {
-            return "one";
-        }
+        return number == 1 ? "it" : "one";
     }
 
+    // guard clause
     private String quantity(int number) {
         if (number == 0) {
             return "no more";
-        } else {
-            return String.valueOf(number);
         }
+
+        return String.valueOf(number);
     }
 
     private String action(int number) {
