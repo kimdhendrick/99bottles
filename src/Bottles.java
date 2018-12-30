c3722c02f6908f712d475b4b173d7664c32f33dc
David Madouros and Kim Hendrick <pair+david@earthvectors.com+kim@earthvectors.com@earthvectors.com>
2018-12-30 11:51:56 -0700

    Listing 5.19: Forward Without Redundancy

--- src/Bottles.java
+++ src/Bottles.java
@@ -21,7 +21,7 @@ class Bottles {
     }
 
     private String container(int number) {
-        return new BottleNumber(number).container(number);
+        return new BottleNumber(number).container();
     }
 
     private String pronoun(int number) {
